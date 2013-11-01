2a6fc81e9b51f3f5459d1f4fca0106e6c238f418
Matt Whiteley <mwhiteley@engineyard.com>
2013-11-01 16:46:26 -0700

    Quick fixes
    - Wrong license
    - Missing test-kitchen support
    - Missing supports in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'libguestfs'
 maintainer       'Engine Yard, Inc.'
 maintainer_email 'cookbooks@engineyard.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures libguestfs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
+supports 'ubuntu', ">= 12.04"
